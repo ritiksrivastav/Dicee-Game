753201ccc4d2d5c5e0697405b64935fd9d4c60c2
ritiksrivastav <47292950+ritiksrivastav@users.noreply.github.com>
2020-09-08 18:47:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var randomNumber1 = Math.random();
 randomNumber1 = Math.floor((randomNumber1*6)+1);
 var randomDice1Image = "dice"+randomNumber1+".png"; 
-var randomImage1source = "images/"+randomDice1Image;
+var randomImage1source =randomDice1Image;
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src",randomImage1source);
 
 var randomNumber2 = Math.random();
 randomNumber2 = Math.floor((randomNumber2*6)+1);
 var randomDice2Image = "dice"+randomNumber2+".png";
-var randomImage2source = "images/"+randomDice2Image;
+var randomImage2source = randomDice2Image;
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src",randomImage2source);
 
